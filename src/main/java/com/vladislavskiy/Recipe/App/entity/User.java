ac908cc3f85a256746e340c1b463ab7927f9bb12
vladi <vladislavskiyproduction@gmail.com>
2023-03-19 23:36:19 +0200

    added for recipes delete, update, add and pages for everyone some fixes in security config

--- src/main/java/com/vladislavskiy/Recipe/App/entity/User.java
+++ src/main/java/com/vladislavskiy/Recipe/App/entity/User.java
@@ -1,14 +1,19 @@
 package com.vladislavskiy.Recipe.App.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 @Entity(name = "users")
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -16,7 +21,33 @@ public class User {
     private String name;
     private String surname;
     private String email;
+    //private boolean enabled = true;
     private String hashPassword;
     @Transient
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Recept> userRecepts;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
 }
